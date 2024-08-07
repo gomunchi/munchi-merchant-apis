30405c7dc587535750e8d18bda261ff65d231580
quochuy278 <quochuy270800@gmail.com>
2024-08-07 16:18:27 +0300

    refactor webhook service with event emit module and socket service

--- src/webhook/webhook.module.ts
+++ src/webhook/webhook.module.ts
@@ -1,21 +1,14 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { BusinessModule } from 'src/business/business.module';
-import { NotificationModule } from 'src/notification/notification.module';
 import { ProviderModule } from 'src/provider/provider.module';
+import { SocketModule } from 'src/socket/socket.module';
 import { UtilsModule } from 'src/utils/utils.module';
+import { ZapierModule } from 'src/zapier/zapier.module';
 import { WebhookController } from './webhook.controller.';
 import { WebhookService } from './webhook.service';
-import { ZapierModule } from 'src/zapier/zapier.module';
 
 @Module({
-  imports: [
-    forwardRef(() => BusinessModule),
-    UtilsModule,
-    ZapierModule,
-    forwardRef(() => NotificationModule),
-    ProviderModule,
-    ZapierModule,
-  ],
+  imports: [BusinessModule, UtilsModule, ZapierModule, SocketModule, ProviderModule],
   controllers: [WebhookController],
   providers: [WebhookService],
   exports: [WebhookService],
