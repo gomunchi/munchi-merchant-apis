a8c69b53f8644cc6d41f173b29392319dbb93eed
quochuy278 <quochuy270800@gmail.com>
2024-09-25 16:44:42 +0300

    fix remove OrderingGuard from socket module

--- src/socket/socket.module.ts
+++ src/socket/socket.module.ts
@@ -5,14 +5,13 @@ import { SocketService } from './socket.service';
 import { AuthenticatedGateway } from './socket.v2.service';
 import { JwtStrategy } from 'src/auth/strategy/jwt.strategy';
 import { JwtGuard } from 'src/auth/guard/jwt.guard';
-import { OrderingGuard } from 'src/auth/guard/ordering.guard';
 import { UserModule } from 'src/user/user.module';
 import { AuthModule } from 'src/auth/auth.module';
 
 @Global()
 @Module({
   imports: [BusinessModule, ProviderModule, UserModule, AuthModule],
-  providers: [SocketService, AuthenticatedGateway, JwtStrategy, JwtGuard, OrderingGuard],
+  providers: [SocketService, AuthenticatedGateway, JwtStrategy, JwtGuard],
   exports: [SocketService],
 })
 export class SocketModule {}
