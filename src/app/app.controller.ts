5046dc5705ec74331f02fb2c1ba1267615aec407
quochuy278 <quochuy270800@gmail.com>
2024-10-18 10:45:38 +0300

    create dummy route for foodora testing

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, ForbiddenException, Get } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Param, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  private logger = new Logger(AppController.name);
   constructor(private readonly appService: AppService) {}
 
   @Get()
   welcome() {
     return this.appService.getHello();
+  }
+
+  @Post('/foodora/notification/order/:remoteId')
+  dummyFoodoraRoute(
+    @Body() foodoraWebhookdata: any,
+    @Req() request: Request,
+    @Param() remoteId: string,
+  ) {
+    this.logger.log(`Received Foodora request headers: ${JSON.stringify(request.headers)}`);
+    this.logger.log(`Received Foodora webhook data: ${JSON.stringify(foodoraWebhookdata)}`);
+    return 'Its just a dummy response for testing purpose';
   }
 }
