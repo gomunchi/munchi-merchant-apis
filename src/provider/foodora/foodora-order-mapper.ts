0a86218b94e43af64b65a7546515fbbee6741215
dmytroivasikiv <dmytroivasikiv@nerdzlab.com>
2024-10-21 11:56:25 +0300

    fix(foodora): fixed bussines identifying

--- src/provider/foodora/foodora-order-mapper.ts
+++ src/provider/foodora/foodora-order-mapper.ts
@@ -33,7 +33,7 @@ export class FoodoraOrderMapperService {
   }
 
   public async mapFoodoraOrderToOrderResponse(foodoraOrder: FoodoraOrder): Promise<OrderResponse> {
-    const businessData = await this.validateBusinessByVenueId(foodoraOrder.platformRestaurant.id);    
+    const businessData = await this.validateBusinessByVenueId(`FI${foodoraOrder.platformRestaurant.id}`);
 
     let deliveryType: number = OrderingDeliveryType.PickUp;
 
