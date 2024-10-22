6cc4b60343ca52dd73ee1243963f3d3a62b3c95a
quochuy278 <quochuy270800@gmail.com>
2024-10-22 15:53:48 +0300

    create OrderPrismaSelectArgs

--- src/provider/provider.type.ts
+++ src/provider/provider.type.ts
@@ -1,3 +1,4 @@
+import { Prisma } from '@prisma/client';
 import { FoodoraOrder } from './foodora/dto/foodora-order-response.dto';
 import { OrderingOrder } from './ordering/dto/ordering-order.dto';
 import { WoltOrderV2 } from './wolt/dto/wolt-order-v2.dto';
@@ -25,3 +26,63 @@ export enum ProviderEnum {
 export type AvailableProvider = ProviderEnum.Wolt | ProviderEnum.Munchi | ProviderEnum.Foodora;
 
 export type ProviderOrder = OrderingOrder | WoltOrder | WoltOrderV2 | FoodoraOrder;
+
+export const OrderPrismaSelectArgs = Prisma.validator<Prisma.OrderInclude>()({
+  business: {
+    select: {
+      publicId: true,
+      name: true,
+      logo: true,
+      email: true,
+      phone: true,
+      description: true,
+      timeZone: true,
+      address: true,
+    },
+  },
+  customer: {
+    select: {
+      name: true,
+      phone: true,
+    },
+  },
+  offers: true,
+  preorder: {
+    select: {
+      preorderTime: true,
+      status: true,
+    },
+  },
+  products: {
+    select: {
+      productId: true,
+      comment: true,
+      name: true,
+      options: {
+        select: {
+          optionId: true,
+          image: true,
+          price: true,
+          name: true,
+          subOptions: {
+            select: {
+              subOptionId: true,
+              name: true,
+              image: true,
+              price: true,
+              position: true,
+              quantity: true,
+            },
+          },
+        },
+      },
+      price: true,
+      quantity: true,
+    },
+  },
+  summary: {
+    select: {
+      total: true,
+    },
+  },
+});
